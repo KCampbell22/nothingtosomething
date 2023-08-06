ddb51c15c73513e9d02704e037b8ce5fee423993
KCampbell22 <ksoup12@gmail.com>
2023-08-06 12:56:59 -0500

    added content, logo to mobile

--- src/About.js
+++ src/About.js
@@ -1,5 +1,28 @@
 import React from "react";
+import "./About.css";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Us</h2>
+      <div className="about-content">
+        <p>
+          We are offering Independent Contractors nationwide delivery services
+          which will include local, state, nationwide and international
+          deliveries with retailers. Our customers will experience efficient and
+          safe deliveries. NTS Commissions LLC wants to take on the issue in the
+          trucking & logistic industry to provide a better experience for
+          employees or contractors by providing competitive benefits through
+          such services as Pensions, 401k, Vision, Dental, Medical, Tuition
+          Reimbursement, Leadership teaching courses and competitive in the
+          market of acquiring and advancing technology to advance the efficiency
+          of the trucking industry. We look to provide more opportunities to
+          truck drivers since they are the backbone of America. As business
+          grows this will also allow you to determine best locations for opening
+          Truck Yards nationwide in different areas which will include miniature
+          homes for optimal rest while working the job.
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/Services.js
+++ src/Services.js
@@ -1,5 +1,17 @@
 import React from "react";
+import "./Services.css";
 
 export default function Services() {
-  return <div>services</div>;
+  return (
+    <div id="services">
+      <h2>Services</h2>
+      <p>
+        Nothing to Something Commissions LLC will provide Independent Contract
+        delivery services for retailers nationwide and in major and minor cities
+        of the United State of America. Delivery service will be done in
+        24ft-26ft Box Truck and 53 ft 18 Wheelers. We deliver all products and
+        have hopes of expanding to cargo and boats as well.
+      </p>
+    </div>
+  );
 }
